afa34cd58c6a6b311dbeb467a05836b8d7130498
timbledum <timbledum@gmail.com>
2018-08-05 19:06:01 +1200

    Add documentation

--- snake.py
+++ snake.py
@@ -1,8 +1,27 @@
+"""Snake implemented with pyxel.
+
+This is the game of snake in pyxel version!
+
+Try and collect the tasty apples without running
+into the side or yourself.
+
+Controls are the arrow keys ← ↑ → ↓
+
+Q: Quit the game
+R: Restart the game
+
+Created by Marcus Croucher in 2018.
+"""
+
 from collections import namedtuple, deque
 from random import randint
 import pyxel
 
-Point = namedtuple("Point", ["x", "y"])
+Point = namedtuple("Point", ["x", "y"])  # Convenience class for coordinates
+
+#############
+# Constants #
+#############
 
 COL_BACKGROUND = 3
 COL_BODY = 11
@@ -28,14 +47,24 @@ LEFT = Point(-1, 0)
 
 START = Point(5, 5 + HEIGHT_SCORE)
 
+###################
+# The game itself #
+###################
 
-class App:
+
+class Snake:
+    """The class that sets up and runs the game."""
+
     def __init__(self):
+        """Initiate pyxel, set up initial game variables, and run."""
+
         pyxel.init(WIDTH, HEIGHT, caption="Snake!", scale=8, fps=22)
         self.reset()
         pyxel.run(self.update, self.draw)
 
     def reset(self):
+        """Initiate key variables (direction, snake, apple, score, etc.)"""
+
         self.direction = RIGHT
         self.snake = deque()
         self.snake.append(START)
@@ -43,7 +72,13 @@ class App:
         self.score = 0
         self.generate_apple()
 
+    ##############
+    # Game logic #
+    ##############
+
     def update(self):
+        """Update logic of game. Updates the snake and checks for scoring/win condition."""
+
         if not self.death:
             self.update_direction()
             self.update_snake()
@@ -57,6 +92,8 @@ class App:
             self.reset()
 
     def update_direction(self):
+        """Watch the keys and change direction."""
+
         if pyxel.btn(pyxel.KEY_UP):
             if self.direction is not DOWN:
                 self.direction = UP
@@ -71,30 +108,44 @@ class App:
                 self.direction = RIGHT
 
     def update_snake(self):
+        """Move the snake based on the direction."""
+
         old_head = self.snake[0]
         new_head = Point(old_head.x + self.direction.x, old_head.y + self.direction.y)
         self.snake.appendleft(new_head)
         self.popped_point = self.snake.pop()
 
     def check_apple(self):
+        """Check whether the snake is on an apple."""
+
         if self.snake[0] == self.apple:
             self.score += 1
             self.snake.append(self.popped_point)
             self.generate_apple()
 
     def generate_apple(self):
+        """Generate an apple randomly."""
+
         x = randint(0, WIDTH - 1)
         y = randint(HEIGHT_SCORE + 1, HEIGHT - 1)
         self.apple = Point(x, y)
 
     def check_death(self):
+        """Check whether the snake has died (out of bounds or doubled up.)"""
+
         head = self.snake[0]
         if head.x < 0 or head.y <= HEIGHT_SCORE or head.x >= WIDTH or head.y >= HEIGHT:
             self.death = True  # Check out of bounds
         elif len(self.snake) != len(set(self.snake)):
             self.death = True  # Check having run into self
 
+    ##############
+    # Draw logic #
+    ##############
+
     def draw(self):
+        """Draw the background, snake, score, and apple OR the end screen."""
+
         if not self.death:
             pyxel.cls(col=COL_BACKGROUND)
             self.draw_snake()
@@ -105,6 +156,8 @@ class App:
             self.draw_death()
 
     def draw_snake(self):
+        """Draw the snake with a distinct head by iterating through deque."""
+
         for i, point in enumerate(self.snake):
             if i == 0:
                 colour = COL_HEAD
@@ -113,11 +166,15 @@ class App:
             pyxel.pix(point.x, point.y, col=colour)
 
     def draw_score(self):
+        """Draw the score at the top."""
+
         score = "{:04}".format(self.score)
         pyxel.rect(0, 0, WIDTH, HEIGHT_SCORE, COL_SCORE_BACKGROUND)
         pyxel.text(1, 1, score, COL_SCORE)
 
     def draw_death(self):
+        """Draw a blank screen with some text."""
+
         pyxel.cls(col=COL_DEATH)
         display_text = TEXT_DEATH[:]
         display_text.insert(1, "{:04}".format(self.score))
@@ -128,8 +185,10 @@ class App:
 
 
 def center_text(text, page_width, char_width=pyxel.constants.FONT_WIDTH):
+    """Helper function for calcuating the start x value for centered text."""
+
     text_width = len(text) * char_width
     return (page_width - text_width) // 2
 
 
-App()
+Snake()
